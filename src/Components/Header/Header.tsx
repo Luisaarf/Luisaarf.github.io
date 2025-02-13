d9a570bcdbbf38195552faea62d9b9bdae49c4a2
Luísa Foppa <lulufoppa@gmail.com>
2025-02-13 20:47:57 -0300

    feat: header shrinking

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext , useEffect, useState} from 'react';
 import { LanguageContext } from '../../translations/Language';
 import './Header.css';
 import LanguageSelector from './LanguageSelector';
@@ -7,22 +7,52 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import translationEn from '../../translations/en.json';
 import translationPt from '../../translations/pt-br.json';
 import docCurriculum from '../../assets/docs/CurriculoLuisaRFoppa.pdf';
+import recruTest from '../../assets/docs/RecruTest.pdf';
   
 const Header = () => {
     const { language } = useContext(LanguageContext);
     const translation = language === 'BR' ? translationPt : translationEn;
+    const [scrolled, setScrolled] = useState(false);
+
+    const loadCurriculum = () => {
+        // if (language !== 'BR') {
+        //     return recruTest ## english curriculum
+        // }
+        return docCurriculum
+    }
+
+    window.onscroll = function() {scrollFunction()};
+
+    const scrollFunction = () => {
+        document.body.scrollTop > 20 || document.documentElement.scrollTop > 20? setScrolled(true) : setScrolled(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', scrollFunction);
+        return () => window.removeEventListener('scroll', scrollFunction);
+    }, []);
+
     return (
-        <div className="header">
+        <div style={{ 
+                height: scrolled ? '20px' : '40px',  
+                transition: 'all 0.3s ease-in-out',
+                backgroundColor: scrolled ? 'rgb(102, 166, 120)' : 'rgb(241, 246, 241)',
+                }} 
+                className="header">
             <div className="head-title">
                 <p>Lu Foppa</p>
                 {/* Deixar imagem ou só texto?  */}
             </div>
-            <nav className="head-nav">
+            <nav style={{ 
+                height: scrolled ? '0px' : '40px',  
+                transition: 'all 0.3s ease-in-out',
+                 visibility: scrolled ? 'hidden' : 'visible',
+                }} className="head-nav">
                 <ul className="head-list">
                     <li className="head-languages-container">
                         <LanguageSelector  />
                     </li>
-                    <li><a href={docCurriculum}  target='_blank'>{translation.Header.curriculum}</a></li>
+                    <li><a href={loadCurriculum()}  target='_blank'>{translation.Header.curriculum}</a></li>
                     <li className="contactme_container">
                         <a href="#contato"><FontAwesomeIcon icon={faEnvelope} /></a>
                         <span className="contactme_caption">{translation.Header.contactcaption}</span>
